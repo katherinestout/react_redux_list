7851e3e4d443e6661e1833ebf69512e83ece9ba9
katherinestout <kjs14@hood.edu>
2019-05-23 22:20:44 -0400

    routing and err

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,51 @@
 const express = require('express');
 const router = express.Router();
 
+//test
 router.get('/', (req, res) => {
     res.send("hello world");
-    if(err){
-        return console.log(err, "there was an error!");
+});
+
+//get all
+router.get('/api/all', (error, res) => {
+  
+    if(error){
+        res.send("error getting all");
+    } else {
+        res.send("get all route");
     }
+      
+});
+
+//add
+router.post('/api/add', function(error, res){
+    if(error){
+        console.log("error adding!");
+    } else{
+        res.send("able to post");
+    }
+  
+});
+
+//delete
+router.delete('/api/delete', function(error, res){
+    if(error) {
+        console.log("error deleting!");
+    } else{
+        res.send("able to delete");
+    }
+
+
+});
+
+//update
+router.put('/api/update', function(error, res){
+    if(error){
+        console.log("error updating!");
+    } else {
+        res.send("able to update");
+    }
+
 });
 
 module.exports = router;
