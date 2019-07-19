a4975188f3b765d3de3d1a723748de2f2c5f6865
katherinestout <kjs14@hood.edu>
2019-07-19 12:50:03 -0400

    no redux

--- client/src/actions/types.js
+++
@@ -1,4 +0,0 @@
-//export const ADD_DONUT = 'ADD_DONUT';
-//export const UPDATE_DONUT = 'UPDATE_DONUT';
-export const GET_DONUTS = 'GET_DONUTS';
-//export const DELETE_DONUT = 'DELETE_DONUT';
--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import donutReducer from './donutReducer';
-
-
-
-export default combineReducers({
-    donut: donutReducer
-});
